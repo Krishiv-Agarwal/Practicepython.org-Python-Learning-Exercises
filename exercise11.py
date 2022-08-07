f89adb7c29edb4176e73bbfc83aef1d04d1d0e1d
Krishiv <krishiv.agarwal@cgpworldwide.com>
2022-08-07 18:24:20 +0530

    Swapped ex11 and 12, finished ex11

--- exercise11.py
+++ exercise11.py
@@ -1,25 +1,34 @@
 #Exercise 11, determine if prime
 #INIT
 
-def first_and_last_finder(list):
-    #Will return the first (1) and the last (-1) terms on the list
-    return  [list[0], list[-1]]
+#Functions
+def prime_number_calc(number):
+    iterator = int()
+    prime_number = False
+
+    #Iterating through all positive integers until the numbers and dividing the number by them
+    #2 as a starting number due to prime numbers being divisble by both 1 and itself.
+    for iterator in range(2, number + 1, 1):
+        if number % iterator != 0:
+            prime_number = False
+        else:
+            prime_number = True
+        return prime_number
 
 #Variables
-user_list = list()
-first_and_last_term = list()
-iterator = int()
+number = int()
+prime_number = bool()
 
-#Inputting user list
-for iterator in range(1,6,1):
-    #Adding user numbers to user list
-    user_list.append(int(input("Enter the next number you want on the list: ")))
-print(user_list)
+#Input
+print("\nWelcome to prime number calculator!")
+print("---------------INIT----------------")
 
-#Generating list with first and last terms
-#appending the returned quantity
-first_and_last_term.append(first_and_last_finder(user_list))
- 
- #Output
-for iterator in first_and_last_term:
-    print(iterator)
+number = int(input("Please enter the number that you want to check is prime: "))
+
+#Checking if prime
+prime_number = prime_number_calc(number)
+
+if prime_number == True:
+    print("\nThe number " + str(number) + " is a prime number.")
+else:
+    print("\nThe number " + str(number) + "is not a prime number.")
---
+++ exercise12.py
@@ -0,0 +1,26 @@
+#Exercise 12, list ends
+#INIT
+
+#Defining the core function
+def first_and_last_finder(list):
+    #Will return the first (1) and the last (-1) terms on the list
+    return  [list[0], list[-1]]
+
+#Variables
+user_list = list()
+first_and_last_term = list()
+iterator = int()
+
+#Inputting user list
+for iterator in range(1,6,1):
+    #Adding user numbers to user list
+    user_list.append(int(input("Enter the next number you want on the list: ")))
+print(user_list)
+
+#Generating list with first and last terms
+#appending the returned quantity
+first_and_last_term.append(first_and_last_finder(user_list))
+ 
+ #Output
+for iterator in first_and_last_term:
+    print(iterator)
