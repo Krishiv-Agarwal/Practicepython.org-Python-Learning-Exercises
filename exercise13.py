c7edc5284aa15fe197965c684343d76c62339df8
Krishiv <krishiv.agarwal@cgpworldwide.com>
2022-08-08 08:45:58 +0530

    Finished ex13

--- exercise13.py
+++ exercise13.py
@@ -1,1 +1,25 @@
-#Exercise 3, 
+#Exercise 13, Fibbonaci Generator
+#INIT
+
+#Functions
+def fibbonaci_generator(limit):
+    #The variable where we store the multiplied numbers
+    value_storage = 1
+    #Regular iteration
+    for iterator in range(1, limit + 1, 1):
+        print("The next fibbonaci value is " + str(value_storage) + " and the next number multiplying is " + str(iterator) + ".")
+        value_storage *= limit
+    #Returning fibbonaci value
+    return value_storage
+
+#Variables
+user_limit = int()
+fibbonaci = int()
+
+#Inputs
+user_limit = int(input("\nPlease enter the number you want to generate a fibbonaci sequence till: "))
+
+#Generating the sequence
+fibbonaci = fibbonaci_generator(user_limit)
+
+print(fibbonaci)
